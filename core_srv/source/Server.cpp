a508cb3a8273dfe632700ef0fbf7eb22073b17f3
hi0ki <ansarimehdi518@gmail.com>
2025-08-13 19:59:46 +0100

    connection done

--- core_srv/source/Server.cpp
+++ core_srv/source/Server.cpp
@@ -4,16 +4,21 @@ Server::Server(int domain, int type, int protocol, int port ,
                                                         u_long ip_add)
 {
     // create socket
-    connection = socket(domain, type, protocol);
+    connection = socket(domain, type, protocol); //  the listening socket.
     if (connection == -1)
         std::cerr << "socket err\n";
     
     //define address
     struct sockaddr_in addr;
     addr.sin_family = domain;
-    addr.sin_addr.s_addr = htonl(ip_add);
+    addr.sin_addr.s_addr = htonl(ip_add); // (host to network long).
     addr.sin_port = htons(port); // change it to bytes with htons bcs maching dont read the decimal
     
+    //////////////////////////// remove it / search about it
+    int opt = 1;
+    setsockopt(connection, SOL_SOCKET, SO_REUSEADDR, &opt, sizeof(opt));
+    ////////////////////////////
+
     // bind
     if (bind(connection, (struct sockaddr *)&addr, sizeof(addr)) == -1)
     {
@@ -26,23 +31,31 @@ Server::Server(int domain, int type, int protocol, int port ,
     if (listen(connection, SOMAXCONN) == -1)
     {
         std::cerr << "listen err\n";
-        close(this->connection);
+        close(connection);
         return ;
     }
     std::cout << "Server listening on port 8080..." << std::endl;
 
 
-    while(1);
     // accept
-    socklen_t client_len = sizeof(addr);
-    int accept_fd = accept(connection, (struct sockaddr*)&addr, &client_len);
-    if (accept_fd == -1)
+    while (true)
     {
-        std::cerr << "accept err\n";
-        close(this->connection);
-        return ;
+
+        // struct sockaddr_in client_addr;
+        // socklen_t client_len = sizeof(client_addr);
+
+        int client_fd = accept(connection, NULL, NULL); // socket in ESTABLISHED state for theat specific client
+        if (client_fd == -1)
+        {
+            std::cerr << "accept err\n";
+            close(connection);
+            return ;
+        }
+        std::cout << "Client connected" << std::endl;
+        send(client_fd, "Hello from server!\n", 20, 0);
+        // std::cout << client_addr.sin_addr.s_addr << std::endl;
+        close (client_fd);
     }
-
-
+    close(connection);
 
 }
