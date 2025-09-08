cd344a675942b2c8c31937f3f04b325a1089bda9
hi0ki <ansarimehdi518@gmail.com>
2025-09-08 19:43:37 +0100

    editing clinet data

--- core_srv/include/ClientData.hpp
+++ core_srv/include/ClientData.hpp
@@ -2,11 +2,24 @@
 
 #include "Server.hpp"
 
-
-
-
 class ClientData
 {
     private:
+        /*
+        
+        This needs a Client class that stores things like:
+        
+            fd,
+
+            request buffer,
+
+            response buffer,
+
+            server it belongs to,
+
+            timestamp of last activity.
+        
+        
+        */
     public:
-};
+}
