c614bbc85b282fea429a8e435c412955974f3245
Hafssa-ANBR <hafssa.2017m5@gmail.com>
2025-09-30 18:34:34 +0100

    some modif

--- CGI/CGI.cpp
+++ CGI/CGI.cpp
@@ -6,8 +6,14 @@
 /*   By: hanebaro <hanebaro@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/09/02 16:00:54 by hanebaro          #+#    #+#             */
-/*   Updated: 2025/09/02 16:01:12 by hanebaro         ###   ########.fr       */
+/*   Updated: 2025/09/30 18:25:49 by hanebaro         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "CGI.hpp"
+
+std::vector<std::string> CGI::cgi_env(const Httprequest &req, const Location &loc)
+{
+    env.push_back("REQUEST_METHOD=" + req.method);
+    
+}
