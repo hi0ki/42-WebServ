58898fdf3f0be1cf853f75bb33c4b4ca25c90808
Hafssa-ANBR <hafssa.2017m5@gmail.com>
2025-09-30 17:00:15 +0100

    create class cgi

--- CGI/CGI.hpp
+++ CGI/CGI.hpp
@@ -6,7 +6,19 @@
 /*   By: hanebaro <hanebaro@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/09/02 16:01:17 by hanebaro          #+#    #+#             */
-/*   Updated: 2025/09/02 16:01:18 by hanebaro         ###   ########.fr       */
+/*   Updated: 2025/09/30 16:54:22 by hanebaro         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
+#pragma once
+#include<iostream>
+#include<vector>
+class CGI
+{
+    private:
+        std::vector<std::string> env;
+    public:
+        std::vector<std::string> cgi_env();
+        
+    
+};
