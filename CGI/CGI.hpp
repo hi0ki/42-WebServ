e2413668b866ba5e561920e082b8c0bf51ddab3e
Hafssa-ANBR <hafssa.2017m5@gmail.com>
2025-09-30 18:34:34 +0100

    some modif

--- CGI/CGI.hpp
+++ CGI/CGI.hpp
@@ -6,19 +6,21 @@
 /*   By: hanebaro <hanebaro@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/09/02 16:01:17 by hanebaro          #+#    #+#             */
-/*   Updated: 2025/09/30 17:00:04 by hanebaro         ###   ########.fr       */
+/*   Updated: 2025/09/30 18:26:34 by hanebaro         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #pragma once
 #include<iostream>
 #include<vector>
+
+#include "../request/request.hpp"
 class CGI
 {
     private:
         std::vector<std::string> env;
     public:
-        std::vector<std::string> cgi_env();
-        
+        std::vector<std::string> cgi_env(const Httprequest &req, const Location &loc);
+        //function to execute
     
 };
