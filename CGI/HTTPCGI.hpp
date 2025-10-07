f1de3693df12d81e44d148110dbb41cfc8e092f1
Hafssa-ANBR <hafssa.2017m5@gmail.com>
2025-10-07 11:49:08 +0100

    CGI

--- CGI/HTTPCGI.hpp
+++ CGI/HTTPCGI.hpp
@@ -6,7 +6,7 @@
 /*   By: hanebaro <hanebaro@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/09/02 16:01:17 by hanebaro          #+#    #+#             */
-/*   Updated: 2025/10/04 15:05:21 by hanebaro         ###   ########.fr       */
+/*   Updated: 2025/10/07 11:47:58 by hanebaro         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -24,5 +24,6 @@ class HTTPCGI
         void cgi_env(Httprequest &req, const Location &loc);
         //function to execute
         // bool canExecuteCGI(Httprequest &req, const Location &loc, std::string& errorMsg);
+        int can_execute(config &conf, int index, Httprequest req);
         std::string execute(const std::string &script_path, const std::string &body);
 };
